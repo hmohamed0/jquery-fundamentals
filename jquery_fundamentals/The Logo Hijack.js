33cad9905816793e937b0c055b96b2e0430e739d
Hodo Mohamed <hmohamed0@gmail.com>
2014-02-19 05:03:57 -0500

    all done

---
+++ jquery_fundamentals/The Logo Hijack.js
@@ -0,0 +1,13 @@
+// Exercises: The DOM
+// EXERCISE: The Logo Hijack
+// No homepage is safe from the logo bandit!
+// Open up www.google.com in Chrome or Firefox, and open up the console.
+// Find the Google logo and store it in a variable.
+// Modify the source of the logo IMG so that it's a Yahoo logo instead.
+// Find the Google search button and store it in a variable.
+// Modify the text of the button so that it says "Yahooo!" instead.
+var google = document.getElementById('hplogo');
+img.width = 700;
+img.src = 'http://www.digitaltrends.com/wp-content/uploads/2013/02/yahoo-voices-hacked-450-000-passwords-posted-online-7169a7e88d.jpg';
+var button = document.getElementById('gbqfba');
+button.innerHTML = 'Yahoooo!';
